46e55bf25ae1e831032a4dce5caf696c2a74909e
Darshan Shaligram <scintilla@gmail.com>
2024-12-29 11:38:00 -0500

    Fix configs

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,1 +1,21 @@
-/home/snark/Dropbox/snarksh/config/wezterm/wezterm.lua
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+
+wezterm.on('gui-startup', function (cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+return {
+  enable_wayland = false,
+  font = wezterm.font('Fira Code'),
+  cell_width = 0.9,
+  cursor_thickness = 4,
+  font_size = 13.5,
+  color_scheme = 'AtomOneLight',
+  hide_tab_bar_if_only_one_tab = true,
+  initial_cols = 400,
+  initial_rows = 150,
+  window_close_confirmation = 'NeverPrompt',
+  window_padding = { left = 3, right = 3, top = 2, bottom = 2 },
+}
